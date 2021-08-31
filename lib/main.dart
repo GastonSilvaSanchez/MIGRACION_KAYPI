92f8199aff07ddb177655206beea77a06b55a9d6
Joaquin Jimenez <jjoaquin253@gmail.com>
2021-08-31 13:31:53 -0400

    Agreagando solicitud de Permisos

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,145 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(LugaresMap());
 
-class MyApp extends StatelessWidget {
+class LugaresMap extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Google ',
-      home: MapSample(),
-    );
+  _LugaresMapState createState() => _LugaresMapState();
+}
+
+class _LugaresMapState extends State<LugaresMap> {
+  late GoogleMapController mapController;
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  final location = new Location().getLocation();
+  late LocationData pinPosicion;
+  late LocationData actualLocation;
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+    _add(LatLng(pinPosicion.latitude!, pinPosicion.longitude!));
   }
-}
-
-class MapSample extends StatefulWidget {
-  @override
-  State<MapSample> createState() => MapSampleState();
-}
-
-class MapSampleState extends State<MapSample> {
-  Completer<GoogleMapController> _controller = Completer();
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
+    final lugarProvider = Provider.of<LugarProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Seleccione una Ubicacion')),
+      floatingActionButton: _floatingButtons(context, lugarProvider),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startDocked,
+      body: _CreateMap(),
+    );
+  }
+
+  Widget _CreateMap() {
+    return FutureBuilder(
+        future: location,
+        builder: (BuildContext context, AsyncSnapshot<LocationData> snapshot) {
+          if (snapshot.hasData) {
+            final pos = snapshot.data; //posicion actual
+
+            pinPosicion = pos!; //posicion del pin
+            actualLocation = pos;
+
+            return SafeArea(
+              child: GoogleMap(
+                onMapCreated: _onMapCreated,
+                myLocationEnabled: true,
+                initialCameraPosition: CameraPosition(
+                    target: LatLng(pos.latitude!, pos.longitude!), zoom: 15),
+                markers: Set<Marker>.of(markers.values),
+              ),
+            );
+          } else {
+            return Center(child: LinearProgressIndicator());
+          }
+        });
+  }
+
+  void _add(LatLng latlng) {
+    final MarkerId markerId = MarkerId('normal');
+
+    final marker = Marker(
+      markerId: markerId,
+      draggable: true,
+      onDragEnd: (latLng) {
+        pinPosicion = new LocationData.fromMap({
+          'latitude': latlng.latitude,
+          'longitude': latlng.longitude,
+        });
+      },
+      position: latlng,
+      infoWindow: InfoWindow(title: 'lugar', snippet: 'Aqui se ubica el sitio'),
+      onTap: () {},
+    );
+
+    setState(() {
+      markers[markerId] = marker;
+    });
+  }
+
+  _floatingButtons(BuildContext context, LugarProvider lugarProvider) {
+    final size = MediaQuery.of(context).size;
+
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: size.height * 0.13),
+          FloatingActionButton.extended(
+              heroTag: UniqueKey(),
+              backgroundColor: Colors.red,
+              foregroundColor: Colors.white,
+              onPressed: () =>
+                  _useActualLocation(context, lugarProvider, pinPosicion),
+              label: Row(
+                children: [
+                  Icon(Icons.pin_drop_rounded),
+                  Text('Ir a la ubicaion del Marcador'),
+                ],
+              )),
+          SizedBox(height: 5),
+          Spacer(),
+        ],
       ),
     );
   }
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  _useActualLocation(BuildContext context, LugarProvider lugarProvider,
+      LocationData location) {
+    return showDialog(
+        builder: (context) => AlertDialog(
+              title: Text('¿Esta es la ubicaion que esperaba?'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                      'Confirme que la ubicacion que carco es el lugar a donde desea ir'),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  child: Text('Si'),
+                  onPressed: () {
+                    lugarProvider.ubication = location;
+                    Navigator.of(context).pop();
+                    return Navigator.of(context).pop();
+                  },
+                ),
+                TextButton(
+                  child: Text('no'),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+              ],
+            ),
+        context: context,
+        barrierDismissible: true);
   }
+}
+
+class LugarProvider {
+  set ubication(LocationData ubication) {}
 }
