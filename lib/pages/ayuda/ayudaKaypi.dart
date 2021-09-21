c249c7c64d388abc2081a60bebad53870e3aa457
axe1619 <mca2019262@est.univalle.edu>
2021-09-21 17:00:00 -0400

    Agregar cards

--- lib/pages/ayuda/ayudaKaypi.dart
+++ lib/pages/ayuda/ayudaKaypi.dart
@@ -13,13 +13,137 @@ class _AyudaKaypiState extends State<AyudaKaypi> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
+          backgroundColor: Colors.indigo.shade900,
           //drawer: MyDrawer(),
           extendBodyBehindAppBar: true,
           appBar: AppBar(backgroundColor: Colors.transparent, elevation: 0, leading: InkWell(
           onTap: () => ZoomDrawer.of(context)!.toggle(),
           child: Icon(Icons.menu, color: Colors.grey[600], size: 28,),),),
-          
-          body: Center(
+
+          body: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(0),
+                  child: Center(
+
+                    child: Wrap(
+
+                      spacing: 5,
+                      runSpacing: 5,
+                      children: <Widget>[
+                        
+                        SizedBox(
+                          width: 200,
+                          height: 200,
+                          child: Card(
+                            color: Colors.indigoAccent.shade400,
+                            child: Padding(
+                              padding: EdgeInsets.all(5.0),
+                              child: Column(
+                                children: <Widget>[
+                                  Image.asset('assets/img/ruta.jpg', width: 150,),
+
+                                  SizedBox(height: 20,),
+
+                                  Text("Rutas de las lineas", style: TextStyle(color: Colors.white, fontSize: 18.0),),
+
+                                  SizedBox(height: 10,),
+
+                                  Text("Busca la ruta favorita", style: TextStyle(color: Colors.white, fontSize: 18.0),),
+
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(
+                          width: 200,
+                          height: 200,
+                          child: Card(
+                            color: Colors.indigoAccent.shade400,
+                            child: Padding(
+                              padding: EdgeInsets.all(5.0),
+                              child: Column(
+                                children: <Widget>[
+                                  Image.asset('assets/img/LineasMicros.jpg', width: 180,),
+
+                                  SizedBox(height: 20,),
+
+                                  Text("Rutas de las lineas", style: TextStyle(color: Colors.white, fontSize: 18.0),),
+
+                                  SizedBox(height: 10,),
+
+                                  Text("Busca la ruta favorita", style: TextStyle(color: Colors.white, fontSize: 18.0),),
+
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(
+                          width: 200,
+                          height: 200,
+                          child: Card(
+                            color: Colors.indigoAccent.shade400,
+                            child: Padding(
+                              padding: EdgeInsets.all(5.0),
+                              child: Column(
+                                children: <Widget>[
+                                  Image.asset('assets/img/ruta.jpg', width: 150,),
+
+                                  SizedBox(height: 20,),
+
+                                  Text("Rutas de las lineas", style: TextStyle(color: Colors.white, fontSize: 18.0),),
+
+                                  SizedBox(height: 10,),
+
+                                  Text("Busca la ruta favorita", style: TextStyle(color: Colors.white, fontSize: 18.0),),
+
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(
+                          width: 200,
+                          height: 200,
+                          child: Card(
+                            color: Colors.indigoAccent.shade400,
+                            child: Padding(
+                              padding: EdgeInsets.all(5.0),
+                              child: Column(
+                                children: <Widget>[
+                                  Image.asset('assets/img/LineasMicros.jpg', width: 180,),
+
+                                  SizedBox(height: 20,),
+
+                                  Text("Rutas de las lineas", style: TextStyle(color: Colors.white, fontSize: 18.0),),
+
+                                  SizedBox(height: 10,),
+
+                                  Text("Busca la ruta favorita", style: TextStyle(color: Colors.white, fontSize: 18.0),),
+
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+
+                      ],
+                  
+                    ),
+                  ),
+
+                )
+              ],
+
+            ),
             
           ),
         ));
