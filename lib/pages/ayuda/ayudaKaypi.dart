0587a64aa33b0a4b20048f4234b05f6edd23284c
axe1619 <mca2019262@est.univalle.edu>
2021-09-21 23:24:50 -0400

    Vista ayuda

--- lib/pages/ayuda/ayudaKaypi.dart
+++ lib/pages/ayuda/ayudaKaypi.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
 
@@ -11,6 +12,7 @@ class AyudaKaypi extends StatefulWidget {
 class _AyudaKaypiState extends State<AyudaKaypi> {
   @override
   Widget build(BuildContext context) {
+    final orientacion = MediaQuery.of(context).orientation;
     return MaterialApp(
         home: Scaffold(
           backgroundColor: Colors.indigo.shade900,
@@ -20,132 +22,87 @@ class _AyudaKaypiState extends State<AyudaKaypi> {
           onTap: () => ZoomDrawer.of(context)!.toggle(),
           child: Icon(Icons.menu, color: Colors.grey[600], size: 28,),),),
 
-          body: SafeArea(
-            child: Column(
+
+          body: Padding(
+            padding: const EdgeInsets.all(0),
+             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(0),
-                  child: Center(
-
-                    child: Wrap(
-
-                      spacing: 5,
-                      runSpacing: 5,
-                      children: <Widget>[
-                        
-                        SizedBox(
-                          width: 200,
-                          height: 200,
-                          child: Card(
-                            color: Colors.indigoAccent.shade400,
-                            child: Padding(
-                              padding: EdgeInsets.all(5.0),
-                              child: Column(
-                                children: <Widget>[
-                                  Image.asset('assets/img/ruta.jpg', width: 150,),
-
-                                  SizedBox(height: 20,),
-
-                                  Text("Rutas de las lineas", style: TextStyle(color: Colors.white, fontSize: 18.0),),
-
-                                  SizedBox(height: 10,),
-
-                                  Text("Busca la ruta favorita", style: TextStyle(color: Colors.white, fontSize: 18.0),),
-
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-
-                        SizedBox(
-                          width: 200,
-                          height: 200,
-                          child: Card(
-                            color: Colors.indigoAccent.shade400,
-                            child: Padding(
-                              padding: EdgeInsets.all(5.0),
-                              child: Column(
-                                children: <Widget>[
-                                  Image.asset('assets/img/LineasMicros.jpg', width: 180,),
-
-                                  SizedBox(height: 20,),
-
-                                  Text("Rutas de las lineas", style: TextStyle(color: Colors.white, fontSize: 18.0),),
-
-                                  SizedBox(height: 10,),
-
-                                  Text("Busca la ruta favorita", style: TextStyle(color: Colors.white, fontSize: 18.0),),
-
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-
-                        SizedBox(
-                          width: 200,
-                          height: 200,
-                          child: Card(
-                            color: Colors.indigoAccent.shade400,
-                            child: Padding(
-                              padding: EdgeInsets.all(5.0),
-                              child: Column(
-                                children: <Widget>[
-                                  Image.asset('assets/img/ruta.jpg', width: 150,),
-
-                                  SizedBox(height: 20,),
-
-                                  Text("Rutas de las lineas", style: TextStyle(color: Colors.white, fontSize: 18.0),),
-
-                                  SizedBox(height: 10,),
-
-                                  Text("Busca la ruta favorita", style: TextStyle(color: Colors.white, fontSize: 18.0),),
-
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-
-                        SizedBox(
-                          width: 200,
-                          height: 200,
-                          child: Card(
-                            color: Colors.indigoAccent.shade400,
-                            child: Padding(
-                              padding: EdgeInsets.all(5.0),
-                              child: Column(
-                                children: <Widget>[
-                                  Image.asset('assets/img/LineasMicros.jpg', width: 180,),
-
-                                  SizedBox(height: 20,),
-
-                                  Text("Rutas de las lineas", style: TextStyle(color: Colors.white, fontSize: 18.0),),
-
-                                  SizedBox(height: 10,),
-
-                                  Text("Busca la ruta favorita", style: TextStyle(color: Colors.white, fontSize: 18.0),),
-
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-
-                      ],
-                  
-                    ),
+              children: <Widget>[ 
+                Expanded(
+                  child:orientacion == Orientation.portrait?  ListView.builder(
+                    itemCount: 5,
+                    itemBuilder: (context, index){
+                      return Center(
+                        child: ItemCard(color: Colors.primaries[index % Colors.primaries.length])
+                      );
+                    }
+                  ) 
+                  : GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                    itemCount: 5,
+                    itemBuilder: (context, index){
+                      return Center(
+                        child: ItemCard(color: Colors.primaries[index % Colors.primaries.length])
+                      );
+                    }, 
                   ),
 
-                )
+                ),
+
+              ]
+            )
+          
+          ),
+          
+        ));
+  }
+}
+
+
+//modificar cards
+class ItemCard extends StatelessWidget {
+
+  final Color color;
+
+  const ItemCard({Key? key, required this.color}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+
+        child: Container(
+          height: 170,
+          width: double.infinity,
+          //modificar colores
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.indigo.shade600,
+                color,
               ],
+              
 
             ),
-            
+
+            borderRadius: BorderRadius.circular(25)
           ),
-        ));
+
+          child: Padding(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              children: <Widget>[
+                Image.asset('assets/img/ruta.jpg', width: 150,),
+
+                SizedBox(height: 20,),
+
+                Text("Rutas de las lineas", style: TextStyle(color: Colors.white, fontSize: 18.0),),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
