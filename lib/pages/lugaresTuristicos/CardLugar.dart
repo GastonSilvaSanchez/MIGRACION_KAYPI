59e949dd58963a5708a9ce82a8ac5e4397e1568c
axelmiranda.845 <axelmiranda.845@gmail.com>
2021-10-06 21:59:50 -0400

    Cambios en la vista lugares turisticos

---
+++ lib/pages/lugaresTuristicos/CardLugar.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_kaypi/pages/lugaresTuristicos/listaLugaresTuristicos.dart';
+
+class CardLugar extends StatefulWidget {
+  //atributo
+  late Lugar _lugar;
+
+  //constructor
+  CardLugar(Lugar lugar) {
+    _lugar = lugar;
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    return CardLugarState(_lugar);
+  }
+
+}
+
+class CardLugarState extends State<CardLugar> {
+
+  late Lugar _lugar;
+
+  //constructor
+  CardLugarState(Lugar lugar) {
+    _lugar = lugar;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        elevation: 0,
+        title: Text(
+          _lugar.titulo,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: ListView.builder(
+                itemCount: 1,
+                itemBuilder: (context, index) {
+                  return Center(
+                      child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Container(
+                      child: Column(
+                        children: [
+                          new Column(
+                            children: [
+                              SizedBox(height: 20),
+                              Hero(
+                                tag: _lugar,
+                                child: Image.asset(
+                                  _lugar.imagen,
+                                  height: 100,
+                                ),
+                              ),
+                              SizedBox(height: 15),
+                              Text(
+                                _lugar.titulo.toUpperCase(),
+                                style: TextStyle(
+                                    color: Colors.indigo.shade700,
+                                    fontSize: 22.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(height: 20),
+                            ],
+                          ),
+                          new Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                _lugar.info,
+                                style: TextStyle(
+                                  color: Colors.grey.shade800,
+                                  fontSize: 16.0,
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                "Funcionalidades",
+                                style: TextStyle(
+                                    color: Colors.indigo.shade700,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(height: 10),
+                              new Column(
+                                  children: List.generate(
+                                      _lugar.listaFuncionalidades.length,
+                                      (index) {
+                                return Container(
+                                  child: new Column(
+                                    children: [
+                                      Text(
+                                        _lugar.listaFuncionalidades[index]
+                                            .toString(),
+                                        style: TextStyle(
+                                          color: Colors.grey.shade800,
+                                          fontSize: 16.0,
+                                        ),
+                                      ),
+                                      SizedBox(height: 20),
+                                      Hero(
+                                        tag: _lugar,
+                                        child: Image.asset(
+                                          _lugar.listaImagenes[index],
+                                          height: 280,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }))
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ));
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/lugaresTuristicos/listaLugaresTuristicos.dart
+++ lib/pages/lugaresTuristicos/listaLugaresTuristicos.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_kaypi/pages/lugaresTuristicos/listaOpciones.dart';
-import 'package:flutter_kaypi/pages/lugaresTuristicos/lugar.dart';
+import 'package:flutter_kaypi/pages/lugaresTuristicos/CardLugar.dart';
 import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
 import 'package:expandable/expandable.dart';
 
@@ -11,45 +10,202 @@ class ListaLugaresTuristicos extends StatefulWidget {
   _ListaLugaresTuristicosState createState() => _ListaLugaresTuristicosState();
 }
 
+
+class Lugar {
+  late String titulo;
+  late String info;
+  late String imagen;
+  late List<String> listaFuncionalidades;
+  late List<String> listaImagenes;
+
+  Lugar(String titulo, String info, String imagen,List<String> listaFuncionalidades, List<String> listaImagenes) {
+    this.titulo = titulo;
+    this.info = info;
+    this.imagen = imagen;
+    this.listaFuncionalidades = listaFuncionalidades;
+    this.listaImagenes = listaImagenes;
+  }
+
+  /*Lugar(String titulo, String info, String imagen){
+    //List<String> listaFuncionalidades, List<String> listaImagenes) {
+    this.titulo = titulo;
+    this.info = info;
+    this.imagen = imagen;
+  }*/
+}
+
+
+
 class _ListaLugaresTuristicosState extends State<ListaLugaresTuristicos> {
   List<Lugar> elementos = new List.empty(growable: true);
 
   _ListaLugaresTuristicosState() {
     elementos.add(new Lugar(
-        "Lugar 1",
-        "Descripción 1 Descripción 1 Descripción 1 Descripción 1 Descripción 1 Descripción 1 Descripción 1 Descripción 1",
-        "assets/img/lugar1.jpg"));
-    elementos
-        .add(new Lugar("Lugar 2", "Descripción 2", "assets/img/lugar1.jpg"));
-    elementos
-        .add(new Lugar("Lugar 3", "Descripción 3", "assets/img/lugar1.jpg"));
+      "Lugar 1",
+      "Descripción 1",
+      "assets/img/lugar1.jpg", 
+      ["Elija un punto de partida (puede ser su misma ubicación u otra diferente seleccionada del mapa).",
+      "Elija un punto de destino,  y la aplicación le mostrará las líneas más cercanas que pueden llevarle a su destino.",
+      "La cobertura muestra el alcance actual que tiene la aplicación."], 
+      ["assets/img/ayudaImg/rutas2.jpeg","assets/img/ayudaImg/rutas3.jpeg","assets/img/ayudaImg/rutas3.1.PNG"]
+    ));
+    elementos.add(new Lugar("Lugar 2", "Descripción 2", "assets/img/lugar1.jpg",
+      ["Elija un punto de partida (puede ser su misma ubicación u otra diferente seleccionada del mapa).",
+      "Elija un punto de destino,  y la aplicación le mostrará las líneas más cercanas que pueden llevarle a su destino.",
+      "La cobertura muestra el alcance actual que tiene la aplicación."], 
+      ["assets/img/ayudaImg/rutas2.jpeg","assets/img/ayudaImg/rutas3.jpeg","assets/img/ayudaImg/rutas3.1.PNG"
+    ]));
+    elementos.add(new Lugar("Lugar 3", "Descripción 3", "assets/img/lugar1.jpg",
+      ["Elija un punto de partida (puede ser su misma ubicación u otra diferente seleccionada del mapa).",
+      "Elija un punto de destino,  y la aplicación le mostrará las líneas más cercanas que pueden llevarle a su destino.",
+      "La cobertura muestra el alcance actual que tiene la aplicación."], 
+      ["assets/img/ayudaImg/rutas2.jpeg","assets/img/ayudaImg/rutas3.jpeg","assets/img/ayudaImg/rutas3.1.PNG"]
+    ));
+  }
+
+
+  Widget ListaOpciones(BuildContext context, int index) {
+    return GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => CardLugar(elementos[index])));
+        },
+        child: Card(
+          color: Colors.transparent,
+          shadowColor: Colors.transparent,
+          margin: EdgeInsets.all(2.5),
+          child: Container(
+              height: 200,
+              width: double.infinity,
+              padding: EdgeInsets.only(left: 20, bottom: 10, top: 10),
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.white, Colors.indigo.shade100],
+                  ),
+                  borderRadius: BorderRadius.circular(25)),
+              child: Row(
+                children: <Widget>[
+                  Hero(
+                    tag: elementos[index],
+                    child: Image.asset(
+                      elementos[index].imagen,
+                      height: 100,
+                      width: 200,
+                    ),
+                  ),
+                  Expanded(
+                    child: new Container(
+                      padding: EdgeInsets.all(20),
+                      child: new Row(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          new Expanded(
+                            child: Text(
+                              elementos[index].titulo,
+                              textAlign: TextAlign.left,
+                              style: new TextStyle(
+                                  fontSize: 18.0,
+                                  color: Colors.grey.shade700,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: "Roboto"),
+                            ),
+                          ),
+                          new Expanded(
+                            child: Text(
+                              elementos[index].info,
+                              style: new TextStyle(
+                                  fontSize: 14.0,
+                                  color: Colors.grey.shade700,
+                                  fontFamily: "Roboto"),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )),
+        ));
   }
 
   @override
   Widget build(BuildContext context) {
+    final orientacion = MediaQuery.of(context).orientation;
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.white,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: InkWell(
+          onTap: () => ZoomDrawer.of(context)!.toggle(),
+          child: Icon(
+            Icons.menu,
+            color: Colors.grey,
+            size: 28,
+          ),
+        ),
+      ),
+      body: Padding(
+          padding: const EdgeInsets.all(0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  child: AnimatedSwitcher(
+                    duration: const Duration(seconds: 2),
+                    transitionBuilder: (widget, animation) {
+                      return ScaleTransition(
+                        scale: animation,
+                        child: widget,
+                      );
+                    },
+                    child: orientacion == Orientation.landscape
+                        ? ListView.builder(
+                          physics: const BouncingScrollPhysics(),
+                          itemCount: elementos.length,
+                          itemBuilder: (context, index) => ListaOpciones(context, index)
+                        )
+                        : GridView.builder(
+                            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                            physics: const BouncingScrollPhysics(),
+                            itemCount: elementos.length,
+                            itemBuilder: (context, index) => ListaOpciones(context, index),
+                          ),
+                  ),
+                ),
+              ])
+      ),
+    ));
+  }
+/*
+  @override
+  Widget build(BuildContext context) {
+    final orientacion = MediaQuery.of(context).orientation;
     return MaterialApp(
         home: Scaffold(
             backgroundColor: Colors.white,
             extendBodyBehindAppBar: true,
             appBar: AppBar(
-              backgroundColor: Colors.indigo.shade400,
+              backgroundColor: Colors.transparent,
               elevation: 0,
-              title: new Text('Lugares turísticos',
-                  style: new TextStyle(
-                      fontWeight: FontWeight.bold, color: Colors.white)),
               leading: InkWell(
                 onTap: () => ZoomDrawer.of(context)!.toggle(),
                 child: Icon(
                   Icons.menu,
-                  color: Colors.white,
+                  color: Colors.grey,
                   size: 28,
                 ),
               ),
             ),
+
             body: ListView.builder(
                 physics: const BouncingScrollPhysics(),
                 itemCount: elementos.length,
-                itemBuilder: (context, index) =>
-                    ListaOpciones(context, index, elementos))));
-  }
+                itemBuilder: (context, index) => ListaOpciones(context, index))));
+  }*/
 }
