6904f57284f0be1cbba1e6b0b62e3774dfa3408f
axelmiranda.845 <axelmiranda.845@gmail.com>
2021-10-19 20:58:02 -0400

    Cambios

--- backend/src/routes/lugaresTuristicos.js
+++ backend/src/routes/lugaresTuristicos.js
@@ -33,6 +33,26 @@ router.get('/api/lugares/create', async (req, res) => {
             ubicacion: [faker.address.latitude(), faker.address.longitude()],
             sitioWeb: "https://cochabambabolivia.net/atractivos/catedral-metropolitana/"
         })
+
+        /*await lugar.create({
+            titulo: "Palacio Portales",
+            portada: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/6c/0c/d7/photo0jpg.jpg?w=1200&h=-1&s=1",
+            categoria: "Museos",
+            informacion:[ 
+                          "Ubicado en la zona Queru Queru, el Palacio Portales fue construido entre 1915 y 1927",
+                          "El arquitecto francés Eugène Bliault diseñó el proyecto del Palacio, y fue ejecutado empleando mano de obra artesanal y utilizando materiales de construcción traídos de Europa",
+                          "El Palacio es un ejemplo de estilo ecléctico, que respondía a comienzos del Siglo XX al gusto de las élites europeas.",
+                          "Sus jardines fueron diseñados por expertos japoneses"
+                        ],
+            imagenes:[ 
+                        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0c/dd/15/3d/20160906-180918-largejpg.jpg?w=1200&h=-1&s=1",
+                        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0c/6e/04/89/img-20160807-wa0017-largejpg.jpg?w=1200&h=-1&s=1",
+                        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/6c/0c/dc/photo5jpg.jpg?w=1200&h=-1&s=1"
+                     ],
+            ubicacion: ["-17.37483521862583", "-66.15294463496637"],
+            sitioWeb: "https://centropatino.fundacionpatino.org/areas/palacio-portales/"
+        })*/
+
     }
     res.json({message: 'Se agrego un nuevo registro'});
 });
