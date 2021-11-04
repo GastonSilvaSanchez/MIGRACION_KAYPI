6c9299dd73ff40904819f5d57e47d0ce556a3c9e
SebastianClavijoA <cag5000404@est.univalle.edu>
2021-11-04 15:05:11 -0400

    Agregando la ventana de configuracion junto a una paleta de colores para la aplicacion

--- lib/pages/configuracion/configuracion.dart
+++ lib/pages/configuracion/configuracion.dart
@@ -12,6 +12,8 @@ class Configuracion extends StatefulWidget {
 
 class _ConfiguracionState extends State<Configuracion> {
   bool modoOscuro = false;
+  String dropdownvalue = 'Español';
+  var items =  ['Español','Ingles','Portugués'];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,38 +33,38 @@ class _ConfiguracionState extends State<Configuracion> {
            ),
            ListTile(
               tileColor: Colors.white,
-              leading:   !modoOscuro
-            ? Icon(
-                Icons.light_mode_rounded,
-                size: 30.0,
-                color: Colors.yellow.shade900,
-              )
-            :Icon(
-                Icons.dark_mode_rounded,
+              leading:  
+           
+            Icon(
+                Icons.language,
                 size: 30.0,
                 color: Colors.blue.shade900,
               ),
              
               title: Text(
-                "Cambiar Tema",
+                "Cambiar idioma",
                 style: TextStyle(
                     fontSize: 18.0,
                     fontWeight: FontWeight.bold,
                     color: Colors.blue.shade900),
               ),
-              trailing:  Switch(
-                      value: modoOscuro,
-                       onChanged: (value) {
-                       setState(() {
-                        modoOscuro = value;
-                        print(modoOscuro);
-                       });
-                      },
-                     inactiveTrackColor: Colors.yellow.shade600,
-                     inactiveThumbColor: Colors.yellow.shade800,
-                      activeTrackColor: Colors.blue.shade900,
-                      activeColor: Colors.blueAccent,
-                     ),
+             trailing:DropdownButton(
+                value: dropdownvalue,
+                style: const TextStyle(color: Colors.indigo,fontWeight: FontWeight.bold),
+                  icon: Icon(Icons.keyboard_arrow_down, color: Colors.blue.shade900,),
+                  items:items.map((String items) {
+                       return DropdownMenuItem(
+                           value: items,
+                           child: Text(items)
+                       );
+                  }
+                  ).toList(),
+                onChanged: (String? newValue) {
+                 setState(() {             
+                 dropdownvalue = newValue!;             
+                 });
+                },
+              ),
            ),
              SizedBox(
                 width: 200,
